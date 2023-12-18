923ce2a602433cc84415dad96b877d01b5f9981c
curlyLasagna <luis.gcodes@gmail.com>
2023-12-18 15:22:17 -0500

    Add TU colors; moving over to fork

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,18 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				gold: '#ffbb00',
+				old_line_gold: '#cc9900',
+				graphite: '#3c3c3c',
+				glen_mist: '#dddddd',
+				black: {
+					tu: '#151500'
+				}
+			}
+		},
+
 	},
 	plugins: [
 	],
